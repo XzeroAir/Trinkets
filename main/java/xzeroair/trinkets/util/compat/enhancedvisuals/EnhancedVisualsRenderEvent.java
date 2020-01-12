99df90625aa3005c2a63091b9390341adc968600
XzeroAir <XzeroAir@gmail.com>
2020-01-12 02:42:37 -0500

    Fixed a Stupid mistake I made

--- main/java/xzeroair/trinkets/util/compat/enhancedvisuals/EnhancedVisualsRenderEvent.java
+++ main/java/xzeroair/trinkets/util/compat/enhancedvisuals/EnhancedVisualsRenderEvent.java
@@ -22,36 +22,49 @@ public class EnhancedVisualsRenderEvent {
 	@SubscribeEvent
 	public void onRenderTick(RenderTickEvent event) {
 		if((mc.player != null) && (mc.player.world != null)) {
-			if(TrinketHelper.AccessoryCheck(mc.player, ModItems.trinkets.TrinketDragonsEye)) {
-				if(TrinketsConfig.compat.toughasnails && Loader.isModLoaded("toughasnails")) {
-					if(VisualManager.getPersitentVisual(ToughAsNailsAddon.heat) != null) {
-						VisualManager.getPersitentVisual(ToughAsNailsAddon.heat).reset();
+			if(TrinketsConfig.compat.enhancedvisuals) {
+				if(TrinketHelper.AccessoryCheck(mc.player, ModItems.trinkets.TrinketDragonsEye)) {
+					if(TrinketsConfig.compat.toughasnails && Loader.isModLoaded("toughasnails")) {
+						if((VisualManager.getPersitentVisual(ToughAsNailsAddon.heat) != null) && VisualManager.getPersitentVisual(ToughAsNailsAddon.heat).type.isEnabled()) {
+							final float intensity = VisualManager.getPersitentVisual(ToughAsNailsAddon.heat).getIntensity(event.renderTickTime);
+							if((intensity > 0f)) {
+								VisualManager.getPersitentVisual(ToughAsNailsAddon.heat).reset();
+							}
+						}
 					}
-				}
-				if((VisualManager.visuals.getValues(VisualCategory.splat) != null) && !(VisualManager.visuals.getValues(VisualCategory.splat).isEmpty())) {
-					for(final Visual v:VisualManager.visuals.getValues(VisualCategory.splat)) {
-						if(v.type == VisualTypeSplat.fire) {
-							v.properties.height = 0;
-							v.properties.width = 0;
+					if((VisualManager.visuals.getValues(VisualCategory.splat) != null) && !(VisualManager.visuals.getValues(VisualCategory.splat).isEmpty())) {
+						for(final Visual v:VisualManager.visuals.getValues(VisualCategory.splat)) {
+							if(v.type == VisualTypeSplat.fire) {
+								System.out.println("This Running?");
+								v.properties.height = 0;
+								v.properties.width = 0;
+							}
 						}
 					}
 				}
-			}
-			if(TrinketHelper.AccessoryCheck(mc.player, ModItems.trinkets.TrinketEnderTiara)) {
-				if(VisualManager.getPersitentVisual(VisualType.slender).getIntensity(event.renderTickTime) > 0f) {
-					VisualManager.getPersitentVisual(VisualType.slender).reset();
+				if(TrinketHelper.AccessoryCheck(mc.player, ModItems.trinkets.TrinketEnderTiara)) {
+					if((VisualManager.getPersitentVisual(VisualType.slender) != null) && VisualManager.getPersitentVisual(VisualType.slender).type.isEnabled()) {
+						final float intensity = VisualManager.getPersitentVisual(VisualType.slender).getIntensity(event.renderTickTime);
+						if((intensity > 0f)) {
+							VisualManager.getPersitentVisual(VisualType.slender).reset();
+						}
+					}
 				}
-			}
-			if(TrinketHelper.AccessoryCheck(mc.player, ModItems.trinkets.TrinketSea) && mc.player.isInWater()) {
-				final float intensity = VisualManager.getPersitentVisual(VisualType.blur).getIntensity(event.renderTickTime);
-				if((intensity > 0f) && (intensity < 20f)) {
-					VisualManager.getPersitentVisual(VisualType.blur).reset();
+				if(TrinketHelper.AccessoryCheck(mc.player, ModItems.trinkets.TrinketSea) && mc.player.isInWater()) {
+					if((VisualManager.getPersitentVisual(VisualType.blur) != null) && VisualManager.getPersitentVisual(VisualType.blur).type.isEnabled()) {
+						final float intensity = VisualManager.getPersitentVisual(VisualType.blur).getIntensity(event.renderTickTime);
+						if((intensity > 0f) && (intensity < 20f)) {
+							VisualManager.getPersitentVisual(VisualType.blur).reset();
+						}
+					}
 				}
-			}
-			if(TrinketHelper.AccessoryCheck(mc.player, ModItems.trinkets.TrinketDamageShield)) {
-				final float intensity = VisualManager.getPersitentVisual(VisualType.blur).getIntensity(event.renderTickTime);
-				if((intensity <= (100*TrinketsConfig.SERVER.DAMAGE_SHIELD.explosion_amount))) {
-					VisualManager.getPersitentVisual(VisualType.blur).reset();
+				if(TrinketHelper.AccessoryCheck(mc.player, ModItems.trinkets.TrinketDamageShield)) {
+					if((VisualManager.getPersitentVisual(VisualType.blur) != null) && VisualManager.getPersitentVisual(VisualType.blur).type.isEnabled()) {
+						final float intensity = VisualManager.getPersitentVisual(VisualType.blur).getIntensity(event.renderTickTime);
+						if((intensity > 0f) && (intensity <= (100*TrinketsConfig.SERVER.DAMAGE_SHIELD.explosion_amount))) {
+							VisualManager.getPersitentVisual(VisualType.blur).reset();
+						}
+					}
 				}
 			}
 		}
